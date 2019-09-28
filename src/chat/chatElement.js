1f3109afae4526fbd95edf5f9a608d966c447369
Marco <marco238cuba@gmail.com>
2019-09-28 04:49:49 +0200

    feat: chat bot routed

--- src/chat/chatElement.js
+++ src/chat/chatElement.js
@@ -37,44 +37,57 @@ class ChatElement extends LitElement {
     ];
   }
 
-  render() {
-    return html`
-          <div class="container">
-            <div class="date-info">
-              <p>Hoy</p>
-            </div>
-            <div class="messages-container">
-              <div class="messages-lines">
-                ${this.printMessages()}
-              </div>
-              <div></div>
-            </div>
-            <div class="message-sender">
-              <div class="input-box">
-                <button>
-                  <img src="/src/svgs/smiley-face.svg" alt="Smiley face">
-                </button>
-                <input type="text" placeholder="Escribe el mensaje...">
-              </div>
-              <div class="chat-buttons">
-                <button>
-                  <img src="/src/svgs/microphone.svg" alt="Microphone">
-                </button>
-                <button>
-                  <img src="/src/svgs/send.svg" alt="Send message">
-                </button>
-              </div>
-            </div>
-
-            <div id="robot">
-              <img src="/src/imgs/robot.png" alt="Robot">
-            </div>
-          </div>
-        `;
+  static get properties() {
+    return {
+      chatType: { type: String }
+    };
   }
 
-  firstUpdated() {
-    // console.log(msgList.messages);
+  constructor() {
+    super();
+    this.chatType = window.location.pathname.split('/')[1];
+  }
+
+  render() {
+    return this.chatType === 'chatbot' ? html `
+            <div class="container">
+              <iframe
+                  allow="microphone;"
+                  src="https://console.dialogflow.com/api-client/demo/embedded/aa774f26-f161-4901-a436-23d6eadd970c">
+              </iframe>
+            </div>
+          `: html`
+            <div class="container">
+              <div class="date-info">
+                <p>Hoy</p>
+              </div>
+              <div class="messages-container">
+                <div class="messages-lines">
+                  ${this.printMessages()}
+                </div>
+                <div></div>
+              </div>
+              <div class="message-sender">
+                <div class="input-box">
+                  <button>
+                    <img src="/src/svgs/smiley-face.svg" alt="Smiley face">
+                  </button>
+                  <input type="text" placeholder="Escribe el mensaje...">
+                </div>
+                <div class="chat-buttons">
+                  <button>
+                    <img src="/src/svgs/microphone.svg" alt="Microphone">
+                  </button>
+                  <button>
+                    <img src="/src/svgs/send.svg" alt="Send message">
+                  </button>
+                </div>
+              </div>
+              <div id="robot">
+                <img src="/src/imgs/robot.png" alt="Robot">
+              </div>
+            </div>
+          `;
   }
 
   printMessages() {
